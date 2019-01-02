d462edd1b0af884467fbcee4c6acb006efcc8046
Tom King <design.tomking@gmail.com>
2019-01-02 14:08:22 -0500

    User can post messages

--- ws.js
+++ ws.js
@@ -2,12 +2,17 @@ var WebSocketServer = require('ws').Server,
   wss = new WebSocketServer({port: 40510})
 
 wss.on('connection', function (ws) {
-  ws.on('message', function (message) {
-    console.log('received: %s', message)
+  ws.on('message', function (chat) {
+    const { msg, isUser } = JSON.parse(chat);
+
+    if (isUser) ws.send(msg);
+
+    console.log('received: %s', msg);
+
   })
 
-  setInterval(
-    () => ws.send(`${new Date()}`),
-    1000
-  )
+  // setInterval(
+  //   () => ws.send(`${new Date()}`),
+  //   1000
+  // )
 })
