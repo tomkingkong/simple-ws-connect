ba3cc16aa399fc2aefaaa9b2a8c1035cc456df61
Tom King <design.tomking@gmail.com>
2019-01-02 14:08:39 -0500

    Renaming

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express');
-var index = require('./ws');
+var ws = require('./ws');
 var app = express();
 
 app.get('/', function (req, res) {
    res.sendFile(__dirname + '/index.html');
 })
+
 app.listen(3000, function () {
-   console.log('Example app listening on port 3000!')
+   console.log('Example app listening on port 3000!');
 })
